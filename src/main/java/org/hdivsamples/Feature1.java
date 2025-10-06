4bef975a286ece00e2a4cff6f9c5c867cf41c5c0
Matthias Kraemer <matthias.kraemer@blackduck.com>
2025-10-06 18:22:53 +0200

    Added Feature 1

---
+++ src/main/java/org/hdivsamples/Feature1.java
@@ -0,0 +1,12 @@
+package org.hdivsamples;
+
+public class Feature1 {
+
+    public static int transform(String s){
+        if (s == null)
+            return s.toString();
+        else
+            return -1;
+    }
+    
+}
